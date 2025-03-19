1f818d2344d2338ecbcec2a3c423d532f107215f
adi-ch2019 <adi.ch2019@outlook.com>
2025-03-19 14:52:20 +0530

    test`1

--- WebApp-Storage-DotNet/Global.asax.cs
+++ WebApp-Storage-DotNet/Global.asax.cs
@@ -12,6 +12,7 @@ namespace WebApp_Storage_DotNet
     {
         protected void Application_Start()
         {
+            
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
